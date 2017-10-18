7e8a4ca8509a79f3a39a6dfc8daf3491e873a02d
tuoyl <tuoyl@hlogin03.ihep.ac.cn>
2017-10-18 16:23:23 +0800

    updated pipline

--- he_craftdata_anal.py
+++ he_craftdata_anal.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i","--input",help="data archived path")
 parser.add_argument("-o","--output",help="products archived path")
 parser.add_argument("-f","--fig",action="store_true",help="plot space craft data")
-parser.add_argument("--rangefile",help="rangefile path of hegtigen")
+parser.add_argument("--rangefile",action="store_true",help="rangefile path of hegtigen is $hxmtanalysis")
 parser.add_argument("--clobber",action="store_true",help="excute hegtigen and hescreen and overwrite existing gti or screens files")
 parser.add_argument("--hxbary",action="store_true",help="carry out hxbary and copy Evt file to local directory")
 args = parser.parse_args()
@@ -19,7 +19,8 @@ aux_dir = product_dir + "/AUX/" # AUX path
 acs_dir = product_dir + "/ACS/" # ACS path
 he_dir = product_dir + "/HE/"   # HE  path
 ehkfilename = aux_dir + "EHK.fits"
-print data_dir,args.input
+print data_dir
+print args.input
 print product_dir
 
 if not os.path.isdir(product_dir):os.system('mkdir -p '+product_dir)
@@ -32,17 +33,29 @@ print he_dir
 print ehkfilename
 
 #read filenames
-v2_flag = commands.getoutput('if [ -f '+data_dir+'/HE/*HE-Evt_FFFFFF_V2* ];then ehco 1;else echo 0;fi')
-if v2_flag == 1:
+v2_flag = commands.getoutput('if [ -f '+data_dir+'/HE/*HE-Evt_FFFFFF_V2* ];then echo 1;else echo 0;fi')
+if v2_flag == '1':
     filename = commands.getoutput('ls ' + data_dir + '/HE/*HE-Evt_FFFFFF_V2*')
 else:
     filename = commands.getoutput('ls ' + data_dir + '/HE/*HE-Evt_FFFFFF_V1*')
-print filename
+
+v2_flag = commands.getoutput('if [ -f '+data_dir+'/ACS/*_Orbit_*V2* ];then echo 1;else echo 0;fi')
+if v2_flag == '1':
+    orbitname = commands.getoutput('ls ' + data_dir + '/ACS/*_Orbit_*V2*')
+else:
+    v1_flag = commands.getoutput('if [ -f '+data_dir+'/ACS/*_Orbit_*V1* ];then echo 1;else echo 0;fi')
+    if v1_flag == '1':
+        orbitname = commands.getoutput('ls ' + data_dir + '/ACS/*_Orbit_*V1*')
+    else:
+        orbitname = commands.getoutput('ls '+data_dir+'/ACS/*_Orbit_*')
+
 hvfilename = commands.getoutput('ls ' + data_dir + '/HE/HXMT*HV_FFFFFF*')
 pmfilename = commands.getoutput('ls ' + data_dir + '/HE/HXMT*PM_FFFFFF*')
 deadfilename = commands.getoutput('ls ' + data_dir + '/HE/HXMT*DTime*')
 tempfilename = commands.getoutput('ls ' + data_dir + '/HE/HXMT*TH*')
 
+print filename
+print orbitname
 print hvfilename
 print pmfilename
 print deadfilename
@@ -53,13 +66,7 @@ if not os.path.isfile(ehkfilename):
     ehkgen(data_dir,product_dir)
 
 # generate data products utilizing HXMT software
-if args.hxbary:
-    if not os.path.isfile(he_dir+'/*HE-Evt*'):
-        cp_text = 'cp -n '+filename+' '+he_dir
-        os.system(cp_text)
-        print cp_text #?????????????????hxbary is undo
-
-if not os.path.isfile(he_dir+'he_pi.fits') or True:#???????????????????always True right now
+if not os.path.isfile(he_dir+'he_pi.fits') or True:#!!!!!!!!!!!!!!!!!!!!!!!!always True right now
     hepical_text = 'hepical evtfile='+filename+' outfile='+he_dir+'he_pi.fits clobber=yes'
     os.system(hepical_text)
     print hepical_text
@@ -68,9 +75,10 @@ if args.clobber:
     ' tempfile='+tempfilename+' outfile='+he_dir+'he_gti.fits ehkfile='+ehkfilename+\
     ' defaultexpr=no ELV=6 COR=10 SAA=yes T_SAA=10 TN_SAA=10 SUN_ANGLE=10'\
     ' MOON_ANGLE=10 ANG_DIST=100 clobber=yes'
-    hegtigen_text = 'hegtigen hvfile='+hvfilename+\
-    ' tempfile='+tempfilename+' outfile='+he_dir+'he_gti.fits ehkfile='+ehkfilename+\
-    ' defaultexpr=yes rangefile='+args.rangefile
+    if args.rangefile:
+        hegtigen_text = 'hegtigen hvfile='+hvfilename+\
+        ' tempfile='+tempfilename+' outfile='+he_dir+'he_gti.fits ehkfile='+ehkfilename+\
+        ' defaultexpr=yes rangefile=/home/hxmt/tuoyl/HXMT_process/hxmtanalysis/HEParamConfig.fits'
     os.system(hegtigen_text) 
     print hegtigen_text
     
@@ -78,6 +86,19 @@ if args.clobber:
     ' baddetfile="" detid="det_id>=0" eventtype=1 anticoincidence=""'
     os.system(hescreen_text)
     print hescreen_text
+
+if args.hxbary:
+    # change he_screen keywords and headfile
+    fparkey_text = 'fparkey Events '+he_dir+'he_screen.fits EXTNAME'
+    print fparkey_text
+    os.system(fparkey_text)
+    cphead_text = 'cphead '+filename+'[Events] '+he_dir+'he_screen.fits[Events]'
+    print cphead_text
+    os.system(cphead_text)
+    # carry out hxbary
+    hxbary_text = 'hxbary '+he_dir+'he_screen.fits '+orbitname+' 228.48175 -59.135822 2'
+    print hxbary_text
+    os.system(hxbary_text)
 
 # read Event data and space craft data 
 raw_data,det_id,channel,pulse_width,acd,event_type = readdata(filename)
