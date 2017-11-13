c6ac75e2459ca2701051c18e68321c740e832528
tuoyl <tuoyl@hlogin03.ihep.ac.cn>
2017-11-13 16:10:15 +0800

    generate pipeline script

--- genscript.py
+++ genscript.py
@@ -4,15 +4,20 @@ import argparse
 '''read data list and generate pipline script'''
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--input",help="data archived path")
-parser.add_argument("-n","--name",help="source name")
+parser.add_argument("-i","--input",help="file to be copied")
+parser.add_argument("-p1","--pathofdatabase",help="paht of database")
+parser.add_argument("-p2","--pathofdata",help="path of archieved data")
 args = parser.parse_args()
+
 filename = args.input
-sourcename = args.name
+path_database = args.pathofdatabase
+path_data = args.pathofdata
+path_pipeline = "/home/hxmt/tuoyl/HXMT_process/hxmtanalysis/he_craftdata_anal.py"
+
 with open(filename,'r')as f:
     file = f.read()
 file = file.split('\n')[0:-1]
 with open(filename+'_script.sh','w')as f:
     for i in file:
-        write_str = '%s %s\n'%(i,i)
+        write_str = 'python %s -i %s -o %s --hxbary --clobber --fig\n'%(path_pipeline, path_database+i, path_data+i)
         f.write(write_str)
