b41b7076ea7ae72c1d4b716a416cb5146995515a
tuoyl <tuoyl@hlogin03.ihep.ac.cn>
2017-11-15 15:58:19 +0800

    updated

--- genscript.py
+++ genscript.py
@@ -12,12 +12,15 @@ args = parser.parse_args()
 filename = args.input
 path_database = args.pathofdatabase
 path_data = args.pathofdata
-path_pipeline = "/home/hxmt/tuoyl/HXMT_process/hxmtanalysis/he_craftdata_anal.py"
+path_pipeline = "he_pipeline.py"
 
 with open(filename,'r')as f:
     file = f.read()
 file = file.split('\n')[0:-1]
-with open(filename+'_script.sh','w')as f:
+file = [ x[x.index('/P01'):-1] for x in file ]
+print file
+new_filename = filename[0:filename.index('.lst')]
+with open(new_filename+'_script.sh','w')as f:
     for i in file:
         write_str = 'python %s -i %s -o %s --hxbary --clobber --fig\n'%(path_pipeline, path_database+i, path_data+i)
         f.write(write_str)
